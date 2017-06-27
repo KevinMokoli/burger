d135114e28ee6f1415ad3891463b9294df36a6bd
Kevin Mokoli <kevinmokoli@Kevin-Mokolis-MacBook-Pro.local>
2017-06-26 23:55:28 -0500

    updated burger docs

--- server.js
+++ server.js
@@ -3,32 +3,32 @@
 // *********************************************************************************
 
 // Dependencies
-// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var orm = require("./config/orm.js")
-
-// Sets up the Express App
-// =============================================================
+var methodOverride = require("method-override");
+var PORT = process.env.PORT || 3000;
 var app = express();
-var PORT = process.env.PORT || 8080;
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + "/public"));
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+var exphbs = require("express-handlebars");
 
-// Static directory
-app.use(express.static("app/public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgerscontrollers.js");
+
+app.use("/", routes);
+app.use("/update", routes);
+app.use("/create", routes);
 
 
-
-
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("Listening on port:%s", PORT);
 });
 
-orm.selectALL("burgers")
+// orm.selectALL("burgers")
